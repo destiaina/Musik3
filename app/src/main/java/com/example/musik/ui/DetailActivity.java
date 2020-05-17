11d6418f65a09f6414febace9ffcb22ff00074db
destiaina <ina.destia1@gmail.com>
2020-05-17 09:01:12 +0700

    Download, Play

--- app/src/main/java/com/example/musik/ui/DetailActivity.java
+++ app/src/main/java/com/example/musik/ui/DetailActivity.java
@@ -10,33 +10,62 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.musik.R;
 
 import java.net.URI;
 
 public class DetailActivity extends AppCompatActivity {
 
-    private Button download;
+    private Button download, audio;
+    TextView artistname, name;
+    ImageView imagee;
+    String artis, namee, image, urlaudio, urldownload;
+    public static final String ARTIST_NAME = "artistname";
+    public static final String NAME = "name";
+    public static final String IMAGE = "image";
+    public static final String AUDIO = "audio";
+    public static final String DOWNLOAD = "download";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         download = findViewById(R.id.dwn);
+        artistname=findViewById(R.id.artist);
+        name=findViewById(R.id.title);
+        imagee=findViewById(R.id.imageView2);
+        audio=findViewById(R.id.audioo);
 
+        Bundle exras=getIntent().getExtras();
+        if(exras!=null){
+            artis=exras.getString(ARTIST_NAME);
+            namee=exras.getString(NAME);
+            image=getIntent().getStringExtra(IMAGE);
+            urlaudio=exras.getString(AUDIO);
+            urldownload=exras.getString(DOWNLOAD);
 
-       // download.getOnClickListener(new View.OnClickListener() {
-       //     @Override
-       //     public void onClick(View v) {
-       //         openWebPage(urlaudio);
-       //     }
-       // });
+        }
+
+        artistname.setText(artis);
+        name.setText(namee);
+        Glide.with(this).asBitmap().load(image).into(imagee);
+
+        audio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage(urlaudio);
+            }
+        });
+
 
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openWebPage(urldownload);
             }
         });
     }
--- app/src/main/java/com/example/musik/ui/MainActivity.java
+++ app/src/main/java/com/example/musik/ui/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.musik.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.example.musik.DaftarActivity;
@@ -18,8 +15,6 @@ import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.startapp.android.publish.adsCommon.StartAppSDK;
 
-import net.khirr.android.privacypolicy.PrivacyPolicyDialog;
-
 public class MainActivity extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
 
@@ -28,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        privacyCheck();
+
         StartAppSDK.init(this, "203904306", true);
         setContentView(R.layout.activity_main);
 
@@ -48,43 +43,5 @@ public class MainActivity extends AppCompatActivity {
     public void handleList(View view) {
         Intent intent = new Intent(MainActivity.this, DaftarActivity.class);
         startActivity(intent);
-    }
-
-    private void privacyCheck() {
-        PrivacyPolicyDialog dialog = new PrivacyPolicyDialog(this,
-                "https://localhost/terms",
-                "https://localhost/privacy");
-
-        dialog.addPoliceLine("This application uses a unique user identifier for advertising purposes, it is shared with third-party companies.");
-        dialog.addPoliceLine("This application sends error reports, installation and send it to a server of the Fabric.io company to analyze and process it.");
-        dialog.addPoliceLine("This application requires internet access and must collect the following information: Installed applications and history of installed applications, ip address, unique installation id, token to send notifications, version of the application, time zone and information about the language of the device.");
-        dialog.addPoliceLine("All details about the use of data are available in our Privacy Policies, as well as all Terms of Service links below.");
-
-        // Title Dialog
-        dialog.setTitle("Terms Of Services");
-
-        // Terms
-        dialog.setTermsOfServiceSubtitle("If you click on (accept), you acknowledge that is makes the bla bla");
-        // Dialog set
-        dialog.setTitleTextColor(Color.parseColor("#222222"));
-        dialog.setAcceptButtonColor(ContextCompat.getColor(this, R.color.colorAccent));
-        dialog.show();
-
-        final Intent intent = new Intent(this, MainActivity.class);
-
-        dialog.setOnClickListener(new PrivacyPolicyDialog.OnClickListener() {
-            @Override
-            public void onAccept(boolean b) {
-                Log.e("SecondActivity", "Policies accepted");
-                startActivity(intent);
-                finish();
-            }
-
-            @Override
-            public void onCancel() {
-                Log.e("SecondActivity", "Policies not accepted");
-                finish();
-            }
-        });
     }
 }
